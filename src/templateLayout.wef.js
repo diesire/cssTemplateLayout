3ff545fc8f4c3ee06ada7685cffe71ea4cb31125
Pablo Escalada Gómez <diesire@gmail.com>
2011-12-18 14:15:46 +0100

    Revert 7e00227c177cca7ba83bff10749d7ce58c88aa40^..HEAD

--- src/templateLayout.wef.js
+++ src/templateLayout.wef.js
@@ -39,7 +39,7 @@ var parser = wef.fn.cssParser; //TODO: loader
 
         transform:function (cssFile) {
 
-            function readFile(url) {
+            function readFile(cssFile) {
                 function html5ReadFile() {
                     var support = window.File && window.FileReader && window.FileList && window.Blob;
                     if (!support) throw "OperationNotSupportedException";
@@ -78,15 +78,15 @@ var parser = wef.fn.cssParser; //TODO: loader
 
                 function ajaxReadFile() {
                     var request = new XMLHttpRequest();
-                    request.open("get", url, false);
+                    request.open("get", cssFile, false);
                     request.send("");
                     return request.responseText;
                 }
 
                 try {
-                    return ajaxReadFile(url);
+                    return ajaxReadFile(cssFile);
                 } catch (e) {
-                    return html5ReadFile(url);
+                    return html5ReadFile(cssFile);
                 }
 
             }
