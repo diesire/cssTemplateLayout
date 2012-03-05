a48cc97c8e855ae8d67fe53689aaaae8c2f6a4a4
diesire <diesire@gmail.com>
2012-03-05 22:03:19 +0100

    Update contributors

--- src/templateLayout.js
+++ src/templateLayout.js
@@ -1,6 +1,10 @@
 /*!
  * CSS Template Layout
- * Copyright (c) 2011 Pablo Escalada
+ * Copyright (c) 2011-2012 Pablo Escalada
+ *
+ * Contributor(s):
+ *   César Acebal
+ *
  * MIT Licensed
  */
 (function (global) {
